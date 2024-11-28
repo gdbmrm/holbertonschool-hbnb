484ef569d203f8e5ea535058ab3d214b26adc01e
gdbmrm <>
2024-11-28 09:22:17 +0000

    ajout fonction getcookie dans script.js

--- PARTIE 4/front/scripts.js
+++ PARTIE 4/front/scripts.js
@@ -31,3 +31,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 })});
+
+function getCookie(name) {
+  // Function to get a cookie value by its name
+  // Your code here
+}
+
+function getCookie(name) {
+  let cookieArr = document.cookie.split(";");
+  
+  for(let i = 0; i < cookieArr.length; i++) {
+      let cookiePair = cookieArr[i].split("=");
+      
+      if(name == cookiePair[0].trim()) {
+          return decodeURIComponent(cookiePair[1]);
+      }
+  }
+  return null;
+}
+
